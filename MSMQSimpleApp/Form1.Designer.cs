04eaeda7f4397c81c63a71b796696c788c98615d
Francis <freesix007@gmail.com>
2016-07-27 12:47:50 +0200

    first commit

---
+++ MSMQSimpleApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace MSMQSimpleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.btn_receive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(134, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Italic);
+            this.textBox2.Location = new System.Drawing.Point(134, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(338, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Message to send";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message to receive";
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(501, 29);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 4;
+            this.btn_send.Text = "&Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // btn_receive
+            // 
+            this.btn_receive.Location = new System.Drawing.Point(501, 99);
+            this.btn_receive.Name = "btn_receive";
+            this.btn_receive.Size = new System.Drawing.Size(75, 23);
+            this.btn_receive.TabIndex = 5;
+            this.btn_receive.Text = "&Receive";
+            this.btn_receive.UseVisualStyleBackColor = true;
+            this.btn_receive.Click += new System.EventHandler(this.btn_receive_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 170);
+            this.Controls.Add(this.btn_receive);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Button btn_receive;
+    }
+}
+
---
+++ MSMQSimpleApp/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace MSMQSimpleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendQueueMessage()
+        {
+            using (MessageQueue data = new MessageQueue())
+            {
+                data.Path = @".\private$\data";
+
+                if(!MessageQueue.Exists(data.Path))
+                {
+                    MessageQueue.Create(data.Path);
+                }
+
+                System.Messaging.Message message = new System.Messaging.Message();
+                message.Body = textBox1.Text;
+                data.Send(message);
+            }
+        }
+
+
+        private void ReceiveQueueMessage()
+        {
+            using (MessageQueue data = new MessageQueue())
+            {
+                data.Path = @".\private$\data";
+                System.Messaging.Message message = new System.Messaging.Message();
+                message = data.Receive(new TimeSpan(0, 0, 5));
+                message.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+
+                string msgToDisplay = message.Body.ToString();
+                textBox2.Text = msgToDisplay;
+            }
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            SendQueueMessage();
+        }
+
+        private void btn_receive_Click(object sender, EventArgs e)
+        {
+            ReceiveQueueMessage();
+        }
+    }
+}
